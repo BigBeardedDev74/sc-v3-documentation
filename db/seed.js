03e8363c240e2d0923a0cc792c6caaa434db867b
BigBeardedDev74 <mikedaugherty@bigbearded.dev>
2024-03-26 10:31:22 -0600

    update seed.js

--- db/seed.js
+++ db/seed.js
@@ -1,28 +1,31 @@
-import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import { ConfigOptions } from "./schema.js";
-import configOptions from "../src/data/configOptions.js";
+import configOptions from "../src/data/configOptions.json" assert { type: "json" };
 import dotenv from "dotenv";
+import * as schema from "./schema.js";
 
 dotenv.config();
 
 // console.log("DB_URL", import.meta.env);
+const sql = neon(
+  "postgresql://mdaugherty:7Mc5TXGOpDFC@ep-white-rain-a4pz73lx.us-east-1.aws.neon.tech/v3docs?sslmode=require",
+  { debug: true }
+);
+const db = drizzle(sql, { schema });
 
 const main = async () => {
-  const sql = neon(
-    "postgresql://mdaugherty:7Mc5TXGOpDFC@ep-white-rain-a4pz73lx.us-east-1.aws.neon.tech/v3docs?sslmode=require",
-    { debug: true }
-  );
-  const db = drizzle(sql);
   const data = [];
 
   for (const option of configOptions) {
     data.push(option);
   }
 
+  // console.log("Data", data);
+  // console.log("db", sql, db);
+
   console.log("Seed start");
 
-  console.log(data);
   await db.insert(ConfigOptions).values(data);
   console.log("Seed done");
 };
