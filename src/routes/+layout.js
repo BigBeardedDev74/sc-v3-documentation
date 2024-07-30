a49d8552efcb1365fb85f3ce0f85bd293edd67d4
BigBeardedDev74 <mikedaugherty@bigbearded.dev>
2024-07-30 10:04:41 -0600

    Remove the layout.js call and just do the server call

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,7 +1,7 @@
-export const load = ({ url, data }) => {
-  const { pathname } = url;
-  return {
-    pathname,
-    ...data,
-  };
-};
+// export const load = ({ url, data }) => {
+//   const { pathname } = url;
+//   return {
+//     pathname,
+//     ...data,
+//   };
+// };
