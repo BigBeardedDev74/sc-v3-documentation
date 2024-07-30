d79d256149b75d26e07f98469b2aaa041e70e0bd
BigBeardedDev74 <mikedaugherty@bigbearded.dev>
2024-07-30 11:45:36 -0600

    adding more configs

--- src/routes/add-config/+layout.server.ts
+++ src/routes/add-config/+layout.server.ts
@@ -3,6 +3,8 @@ import { redirect } from "@sveltejs/kit";
 export const load = async (event) => {
   const session = await event.locals.auth();
 
+  console.log("page layout", session);
+
   if (!session) {
     throw redirect(303, "/auth/signin");
   }
