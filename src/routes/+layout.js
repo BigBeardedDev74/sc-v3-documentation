2fa89d0a0e88695edc85acc2da591280a4ce3f07
BigBeardedDev74 <mikedaugherty@bigbearded.dev>
2024-08-02 08:14:35 -0600

    add load to page.js

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,8 +1,3 @@
-/** @type {import('@sveltejs/adapter-vercel').Config} */
-export const config = {
-  runtime: "nodejs18.x",
-};
-
 export const load = ({ url, data }) => {
   const { pathname } = url;
   return {
--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,1 +1,10 @@
 export const prerender = true;
+
+export const load = ({ url, data }) => {
+  const { pathname } = url;
+  console.log("pathname", data);
+  return {
+    pathname,
+    ...data,
+  };
+};
