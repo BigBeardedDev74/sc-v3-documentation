19590312c76d38c00a328341b4dc478ad6f809ea
BigBeardedDev74 <mikedaugherty@bigbearded.dev>
2024-08-02 07:35:05 -0600

    add config edge to layout - just a test

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,3 +1,8 @@
+/** @type {import('@sveltejs/adapter-vercel').Config} */
+export const config = {
+  runtime: "edge",
+};
+
 export const load = ({ url, data }) => {
   const { pathname } = url;
   return {
