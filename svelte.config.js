7e38393dae52c0e8a76098a36a09196469b7b849
BigBeardedDev74 <mikedaugherty@bigbearded.dev>
2024-08-02 07:29:10 -0600

    roll back to version 4 adapter

--- svelte.config.js
+++ svelte.config.js
@@ -5,9 +5,6 @@ import preprocess from "svelte-preprocess";
 const config = {
   preprocess: preprocess({ postcss: true }),
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
     alias: {
       $lib: "./src/lib",
