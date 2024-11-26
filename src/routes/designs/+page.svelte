177d7a6e305a149846101d7597464bcdfc9cc789
BigBeardedDev74 <mikedaugherty@bigbearded.dev>
2024-11-26 15:31:05 -0700

    add borders

--- src/routes/designs/+page.svelte
+++ src/routes/designs/+page.svelte
@@ -30,7 +30,16 @@
   .sitesContainer {
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-    gap: 1rem;
+    gap: 1.5rem;
+  }
+  .site {
+    border: 1px solid var(--textColor);
+    border-radius: 10px;
+    overflow: hidden;
+    background-color: var(--bgColor);
+    p {
+      margin-inline: 1em;
+    }
   }
   .siteImage {
     width: 100%;
